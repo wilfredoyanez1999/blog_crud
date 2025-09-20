31c048576096cb6ffa0cf2010572ca3251c5da77
Wilfredo Yanez <wilfredo123alejandro17@gmail.com>
2025-09-20 18:08:15 -0400

    base-html

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from .models import Post
+
+
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_at', 'updated_at')
-    search_fields = ('title', 'content', 'author__username')
-    list_filter = ('created_at', 'author')
-    ordering = ('-created_at',)
+    list_display = ("title", "author", "created_at", "updated_at")
+    search_fields = ("title", "content", "author__username")
+    list_filter = ("created_at", "author")
+    ordering = ("-created_at",)
 
 
-
-admin.site.register(Post, PostAdmin)  
-
+admin.site.register(Post, PostAdmin)
--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
 
+
 class PostListView(ListView):
     model = Post
     template_name = "post-list.html"
-    
+
+
 class PostDetailView(DetailView):
     model = Post
     template_name = "post-detail.html"
-    
